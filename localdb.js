b65de7e4cda61b63923b0a5ec4c280552651cb8f
iniya133 <nishakrajan@gmail.com>
2020-06-24 19:56:37 +0530

    added word

--- localdb.js
+++ localdb.js
@@ -30,6 +30,6 @@ const db = {
   an: { chunks: ['a', 'n'], phones: ['AE', 'N'] },
   was: { chunks: ['w', 'a', 's'], phones: ['W', 'AA', 'Z'] },
   we: { chunks: ['w', 'e'], phones: ['W', 'IY'] },
-
+  iniya: { chunks: ['i', 'n','i', 'y','a'], phones: ['IY', 'N','IY', 'Y','AA'] },
 };
 export default db;
